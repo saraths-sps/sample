3e377ecf05680047897b69e5c86ebf6e63597d8d
Ubuntu <ubuntu@ip-172-31-12-55.ap-south-1.compute.internal>
2022-10-11 13:05:00 +0000

    merged

--- sample.java
+++ sample.java
@@ -2,6 +2,10 @@
 
 public class HelloWorld {
     public static void main(String[] args) {
+<<<<<<< HEAD
     System.out.println("changed code"); 
+=======
+        System.out.println("sample pgm"); 
+>>>>>>> parent of 06fcee8 (Update sample.java)
     }
 }
